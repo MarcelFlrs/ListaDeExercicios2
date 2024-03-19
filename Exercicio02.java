7f84419fdbe07aa2f0e61c4b1d5563a10c9d860d
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-19 20:02:28 -0300

    Exercício 02

---
+++ Exercicio02.java
@@ -0,0 +1,23 @@
+public class Exercicio02 {
+    public static void executar() {
+        int vetor[] = new int[Prompt.lerInteiro("Digite o tamanho do vetor: ")];
+
+        System.out.println("Digite 5 números: \n");
+        int soma = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = Prompt.lerInteiro((i+1) + "° número:");
+        }
+
+        for (int i = 0; i < vetor.length; i++) {
+            if(vetor[i] > 0){
+                System.out.println("O número da posição " + i +" é positivo");
+            }else if(vetor[i] < 0){
+                System.out.println("O número da posição " + i +" é negativo");
+            }else if(vetor[i] == 0){
+                System.out.println("O número da posição " + i + " é igual a zero");
+            }
+        }
+
+
+    }
+}
