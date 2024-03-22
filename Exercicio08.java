ffd2da41b70df4092b41af3c9f481e3b27c5a040
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-22 09:55:45 -0300

    Exercício 08

--- Exercicio08.java
+++ Exercicio08.java
@@ -1,24 +1,29 @@
-import java.util.PropertyPermission;
-
 public class Exercicio08 {
     public static void executar() {
         int A[] = new int[5];
         int B[] = new int[5];
         int C[] = new int [10];
+
         System.out.println("Digite 5 números para o vetor A");
         for (int i = 0; i < A.length; i++) {
-            A[i] = Prompt.lerInteiro((i+1) +"° número do vetor");
+            A[i] = Prompt.lerInteiro("Valor do "+ (i+1) +"° número do vetor");
         }
         System.out.println("Digite mais 5 números para o vetor B");
         for (int i = 0; i < B.length; i++) {
-            B[i] = Prompt.lerInteiro((i+1) +"° número do vetor");
+            B[i] = Prompt.lerInteiro("Valor do "+ (i+1) +"° número do vetor");
         }
 
         for (int i = 0; i < A.length; i++) {
-            C[i] = A[i] + B[i];
+            C[i] = A[i];
         }
+
+        for (int i = 0; i < B.length; i++) {
+            C[A.length + i] = B[i];
+        }
+
+        System.out.println("Vetores concatenados ");
         for (int i = 0; i < C.length; i++) {
-            System.out.println(C[i]);
+            System.out.print(C[i]+ " ");
         }
     }
 }
